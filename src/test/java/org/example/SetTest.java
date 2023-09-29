2423d41770bd18e11308046ed742d9d21c1d3872
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-29 21:46:09 +0900

    test: ParameterizedTest작성

--- src/test/java/org/example/SetTest.java
+++ src/test/java/org/example/SetTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,14 +17,27 @@ public class SetTest {
     void setUp() {
         numbers = new HashSet<>();
         numbers.add(1);
+        numbers.add(1);
         numbers.add(2);
         numbers.add(3);
-        numbers.add(4);
     }
 
     @Test
     void size() {
         int sizeOfNum = numbers.size();
-        assertThat(sizeOfNum).isEqualTo(4);
+        assertThat(sizeOfNum).isEqualTo(3);
+    }
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isContains_ShouldReturnTrueForContainsNumber(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
