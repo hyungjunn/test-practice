6002f4638c7935e80451ed47c831f7f1dd4e71c9
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-29 16:52:24 +0900

    test : split 학습테스트 작성

--- src/test/java/org/example/StringTest.java
+++ src/test/java/org/example/StringTest.java
@@ -12,4 +12,14 @@ public class StringTest {
         String actual = "velog".replace("v", "b");
         assertThat(actual).isEqualTo("belog");
     }
+
+    @Test
+    @DisplayName("split 학습테스트")
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+
+        String[] value = "1".split(",");
+        assertThat(value).contains("1");
+    }
 }
