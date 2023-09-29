6c66b26781e3d0fffadc3f7e3ef157b78a407800
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-29 16:53:54 +0900

    test : indexof 학습테스트 작성

--- src/test/java/org/example/StringTest.java
+++ src/test/java/org/example/StringTest.java
@@ -30,4 +30,16 @@ public class StringTest {
         String value2 = value1.concat("456");
         assertThat(value2).isEqualTo("123456");
     }
+
+    @Test
+    @DisplayName("indexof 학습테스트")
+    void indexOf() {
+        String name = "제임스고슬링";
+
+        int index1 = name.indexOf("임");
+        assertThat(index1).isEqualTo(1);
+
+        int index2 = name.indexOf("고");
+        assertThat(index2).isEqualTo(3);
+    }
 }
