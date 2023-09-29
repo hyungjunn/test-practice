27910d0a87311c630ccb29289fb183a4f612be72
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-09-29 23:09:49 +0900

    test: ParameterizedTest작성, @CsvSource이용

--- src/test/java/org/example/SetTest.java
+++ src/test/java/org/example/SetTest.java
@@ -3,12 +3,14 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -39,5 +41,11 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void isContains_ShouldReturnTrueForContainsNumber(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
+    void contains_ShouldContains(int input, boolean isContains){
+        assertEquals(numbers.contains(input), isContains);
     }
 }
