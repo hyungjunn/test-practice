b236b47d2d79d705f36b6e735c3c9114a0e591b4
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-10-04 21:22:56 +0900

    test: push테스트코드 추가

--- src/test/java/org/example/livestudy/ListNodeStackTest.java
+++ src/test/java/org/example/livestudy/ListNodeStackTest.java
@@ -2,14 +2,20 @@ package org.example.livestudy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListNodeStackTest {
 
-    ListNode head = new ListNode(1);
+    ListNode head = new ListNode(0);
+    ListNode first = new ListNode(10);
 
     @Test
     void push() {
+        head.add(first, head, 1);
+        assertThat(head.next).isEqualTo(first);
     }
 
     @Test
