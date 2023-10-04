fc2e8270d5e8d69970852045ea57dbffa666c479
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-10-04 20:39:56 +0900

    feat: ifFull() 구현

--- src/main/java/org/example/livestudy/ListNodeStack.java
+++ src/main/java/org/example/livestudy/ListNodeStack.java
@@ -1,4 +1,38 @@
 package org.example.livestudy;
 
+/**
+ * 과제 4. 앞서 만든 ListNode를 사용해서 Stack을 구현하세요.
+ * ListNode head를 가지고 있는 ListNodeStack 클래스를 구현하세요.
+ * void push(int data)를 구현하세요.
+ * int pop()을 구현하세요.
+ */
 public class ListNodeStack {
+
+    private int stackSize;
+
+    private int count;
+
+    ListNode head;
+
+    private boolean isFull(){
+        if (count == stackSize) {
+            return true;
+        }
+        return false;
+    }
+
+    void push(int data) {
+        // 1. 가득 차있을 때
+        if (isFull()) {
+
+        }
+        // 2. 가득 차있지 않을 때
+    }
+
+    int pop() {
+        // 1. 전부 비어 있을 때
+        // 2. 전부 비어 있지 않을때
+        return 0;
+    }
+
 }
---
+++ src/test/java/org/example/livestudy/ListNodeStackTest.java
@@ -0,0 +1,18 @@
+package org.example.livestudy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListNodeStackTest {
+
+    ListNode head = new ListNode(1);
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void pop() {
+    }
+}
