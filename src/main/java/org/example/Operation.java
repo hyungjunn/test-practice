a756c4620b8f47d16e0f9758d8a3a95e94bcda22
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-10-05 12:05:16 +0900

    feat: Operation 열거형으로해보기

--- src/main/java/org/example/Operation.java
+++ src/main/java/org/example/Operation.java
@@ -1,3 +1,32 @@
 package org.example;
 
+// 3. 기능별 옵션
+// enum타입으로 switch문에서 operator 정리
+public enum Operation {
+    PLUS("+") {
+        public double apply(double x, double y) {return x + y;}
+    },
+    MINUS("-") {
+        public double apply(double x, double y) {return x - y;}
+    },
+    TIMES("*") {
+        public double apply(double x, double y) {return x * y;}
+    },
+    DIVIDEBY("/") {
+        public double apply(double x, double y) {return x / y;}
+    };
+
+    // symbol을 숨기기 위해 private, 값 변경을 하지 못하게 하기위해 final
+    private final String symbol;
+
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public abstract double apply(double x, double y);
+
+    @Override
+    public String toString() { return symbol; }
+}
+
 
