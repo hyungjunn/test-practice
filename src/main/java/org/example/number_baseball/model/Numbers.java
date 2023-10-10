4d48986b53784e091221e269eadfe1a742e10d0d
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-10-10 19:37:54 +0900

    feat: 스트라이크 개수 세기

--- src/main/java/org/example/number_baseball/model/Numbers.java
+++ src/main/java/org/example/number_baseball/model/Numbers.java
@@ -12,6 +12,9 @@ public class Numbers {
     private static final int MIN_NUMBERS = 1;
     private static final int MAX_NUMBERS = 9;
 
+    private int strikeCount;
+    private int ballCount;
+
     public List<Integer> generateComputerNumbers() {
         List<Integer> computerNumbers = new ArrayList<>();
         for (int i = 0; i < RANDOM_NUMBERS_SIZE; i++) {
@@ -40,16 +43,6 @@ public class Numbers {
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
-
-    // 숫자가 1~9만 가능
-//    private boolean validatePlayerNumber(String input) {
-//        for (int i = 0; i < input.length(); i++) {
-//            if (isBetween1And9(input.charAt(i))) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
 
     // 숫자가 1~9사이인지
     private boolean isBetween1And9(String input) {
@@ -91,4 +84,13 @@ public class Numbers {
         }
     }
 
+    // 스트라이크 개수 세기
+    private void countStrike(int computerNumber, int playerNumber) {
+        if (computerNumber == playerNumber) {
+            strikeCount++;
+        }
+    }
+
+
 }
+
