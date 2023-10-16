2c39c979151f69b49ebc8af1d657674cd260c0c5
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-10-16 13:52:13 +0900

    feat(baseball): askNumbers구현

--- src/main/java/org/example/baseball/Application.java
+++ src/main/java/org/example/baseball/Application.java
@@ -1,7 +1,5 @@
 package org.example.baseball;
 
-import org.example.baseball.domain.Judgement;
-import org.example.baseball.domain.NumberGenerator;
 import org.example.baseball.domain.Referee;
 
 import java.util.ArrayList;
@@ -12,40 +10,21 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
 
-//        NumberGenerator generator = new NumberGenerator();
-//
-//        List<Integer> numbers = generator.createRandomNumbers();
-//
-//        System.out.println(numbers);
-
-//        Judgement judgement = new Judgement();
-//
-//        List<Integer> computer = new ArrayList<>();
-//
-//        computer.add(9);
-//        computer.add(8);
-//        computer.add(7);
-//
-//        List<Integer> player = new ArrayList<>();
-//
-//        player.add(2);
-//        player.add(3);
-//        player.add(4);
-//
-//        System.out.println(judgement.correctCount(computer, player));
-
-//        Judgement judgement = new Judgement();
-//
-//        System.out.println(judgement.correctCount(Arrays.asList(1,2,3), Arrays.asList(2, 3, 4)));
-//
-//        System.out.println(judgement.hasPlace(Arrays.asList(1,2,3), 1, 3));
-
-//        Scanner scanner = new Scanner(System.in);
-//        int input1 = scanner.nextInt();
-//        int input2 = scanner.nextInt();
-//        int input3 = scanner.nextInt();
-
         Referee referee = new Referee();
         System.out.println(referee.compare(Arrays.asList(3, 4, 5), Arrays.asList(3, 4, 5)));
+
     }
+
+    public static List<Integer> askNumbers() {
+        System.out.print("숫자를 입력해주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
+    }
+
 }
